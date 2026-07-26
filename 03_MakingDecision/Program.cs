cd49f02eaf958bb460e3f9105b6d2c5ed6c8f04d
Emirhan Sartik <sartikemirhan@gmail.com>
2026-07-26 16:22:51 +0300

    Keep Practice

---
+++ 03_MakingDecision/Program.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_MakingDecision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region İf Else Char
+            //int number;
+            //Console.Write("Sayıyı Giriniz: ");
+            //number = int.Parse(Console.ReadLine());
+
+            //if (number == 5)
+            //{
+            //    Console.WriteLine("Sayı doğru");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Sayı hatalı");
+            //}
+
+            //Console.WriteLine("****** C#  Eğitim Kampı Restoran  ******");
+            //Console.WriteLine();
+
+            //Console.WriteLine("----------------------------------------");
+
+            //Console.WriteLine("1-Ana Yemekler");
+            //Console.WriteLine("2-Çorbalar");
+            //Console.WriteLine("3-Pizzalar");
+            //Console.WriteLine("4-İçecekler");
+            //Console.WriteLine("5-Tatlılar");
+
+            //Console.WriteLine("----------------------------------------");
+            //Console.WriteLine();
+
+            //string menuItem;
+
+            //Console.Write("Detayını görmek istediğiniz menü seçimi: ");
+            //menuItem = Console.ReadLine();
+
+            //if (menuItem == "1")
+            //{
+            //    Console.WriteLine();
+            //    Console.WriteLine("--------------------Ana Yemekler--------------------");
+            //    Console.WriteLine();
+            //    Console.WriteLine("1-Köri Soslu Tavuk");
+            //    Console.WriteLine("2-Kızartma Tabağı");
+            //    Console.WriteLine("3-Fasulye Pilav");
+            //    Console.WriteLine("4-Fırında Somon");
+            //    Console.WriteLine("5-Patlican Musakka");
+            //    Console.WriteLine("--------------------Ana Yemekler--------------------");
+
+            //    Console.WriteLine();
+            //}
+
+            //if (menuItem == "2")
+            //{
+            //    Console.WriteLine();
+            //    Console.WriteLine("--------------------Çorbalar--------------------");
+            //    Console.WriteLine();
+            //    Console.WriteLine("1-Mercimek Çorbası");
+            //    Console.WriteLine("2-EzoGelin Çorba");
+            //    Console.WriteLine("--------------------Çorbalar--------------------");
+
+            //    Console.WriteLine();
+            //}
+
+            //if (menuItem == "3")
+            //{
+            //    Console.WriteLine();
+            //    Console.WriteLine("--------------------Pizzalar--------------------");
+            //    Console.WriteLine();
+            //    Console.WriteLine("1-Margarita");
+            //    Console.WriteLine("2-Akdeniz Pizza");
+            //    Console.WriteLine("2-Tavuklu Pizza");
+            //    Console.WriteLine("--------------------Pizzalar--------------------");
+
+            //    Console.WriteLine();
+            //}
+
+            //if (menuItem == "4")
+            //{
+            //    Console.WriteLine();
+            //    Console.WriteLine("--------------------İçecekler--------------------");
+            //    Console.WriteLine();
+            //    Console.WriteLine("1-Su");
+            //    Console.WriteLine("2-Cola");
+            //    Console.WriteLine("2-Ayran");
+            //    Console.WriteLine("--------------------İçecekler--------------------");
+
+            //    Console.WriteLine();
+            //}
+
+            //if (menuItem == "5")
+            //{
+            //    Console.WriteLine();
+            //    Console.WriteLine("--------------------Tatlılar--------------------");
+            //    Console.WriteLine();
+            //    Console.WriteLine("1-Triliçe");
+            //    Console.WriteLine("2-KazanDibi");
+            //    Console.WriteLine("3-Fırında Sütlaç");
+            //    Console.WriteLine("--------------------Tatlılar--------------------");
+
+            //    Console.WriteLine();
+            //}
+
+
+
+
+            #endregion
+
+            #region Switch Case
+
+            //Console.Write("Lütfen  Ay Girişi Yapınız: ");
+            //int monthNumber = int.Parse(Console.ReadLine());
+
+            //switch (monthNumber)
+            //{
+            //    case 1: Console.Write("Ocak");break;
+            //    case 2: Console.Write("Şubat");break;
+            //    case 3: Console.Write("Mart");break;
+            //    case 4: Console.Write("Nisan");break;
+            //    case 5: Console.Write("Mayıs");break;
+            //    case 6: Console.Write("Haziran");break;
+            //    case 7: Console.Write("Temmuz");break;
+            //    case 8: Console.Write("Ağustos");break;
+            //    case 9: Console.Write("Eylül");break;
+            //    case 10: Console.Write("Ekim");break;
+            //    case 11: Console.Write("Kasım");break;
+            //    case 12: Console.Write("Aralık");break;
+            //    default: Console.Write("Hatalı Veri Girişi"); break;
+            //}
+
+            #endregion
+
+            #region Switch Case ile Hesap Makinası
+
+            //int number1, number2, result;
+            //char symbol;
+
+            //Console.Write("1.Sayıyı Giriniz: ");
+            //number1 = int.Parse(Console.ReadLine());
+
+            //Console.Write("2.Sayıyı Giriniz: ");
+            //number2 = int.Parse(Console.ReadLine());
+
+            //Console.Write("Sembol Atamasını Yapınız: ");
+            //symbol = Char.Parse(Console.ReadLine());
+            //switch (symbol)
+            //{
+            //    case '+':
+            //        result = number1 + number2;
+            //        Console.WriteLine("Toplam : " + result);
+            //        break;
+
+            //    case '-':
+            //        result = number1 - number2;
+            //        Console.WriteLine("Fark : " + result);
+            //        break;
+
+            //    case '*':
+            //        result = number1 * number2;
+            //        Console.WriteLine("Çarpım : " + result);
+            //        break;
+
+            //    case '/':
+            //        result = number1 / number2;
+            //        Console.WriteLine("Bölüm : " + result);
+            //        break;
+
+            //}
+
+
+
+            #endregion
+
+
+            Console.Read();
+        }
+    }
+}
---
+++ 04_Loops/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region For Döngüsü
+
+            //int i;
+
+            //for (i=1; i<=5; i++)
+            //{
+            //    Console.WriteLine("C# Eğitim Kampı");
+            //}
+
+
+            //Console.Write("Lütfen ekrana yazılmasını istediğiniz adresi giriniz: ");
+            //int finishValue = int.Parse(Console.ReadLine());
+
+            //for (int i = 1; i <= finishValue; i++) { 
+
+            //Console.WriteLine("Yaşasın Cumhuriyet");   
+
+            //}
+
+
+
+
+
+            #endregion
+
+            #region For Döngüsü ile Karar Yapıları 
+
+            //for (int i = 1; i <= 100; i++) { 
+
+            //    if (i % 5 == 0 )
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+
+            //}
+
+
+            //int totalValue = 0;
+
+            //for (int i = 1; i <= 10; i++) { 
+
+            //    totalValue += i;   
+            //}
+            //Console.WriteLine(totalValue);
+
+
+            #endregion
+
+
+            #region While Döngüsü 
+
+            int i = 1;
+            while (i <=10 ) {
+
+                    Console.WriteLine(i);
+                i++;
+            }
+
+
+            #endregion
+
+
+            #region Örnek Sınav Sorusu
+            // Klavyeden girilen 3 basamaklı sayının basamakları toplamını hesaplayan kodu yazınız.
+
+            Console.Write("Sayı girin: ");
+            int number = int.Parse( Console.ReadLine());
+            int ones, tens, hundreds;
+            int sum;
+
+            ones = number % 10;
+            tens = (number % 100) / 10;
+            hundreds = number / 100; //4.56 --> 4
+
+            Console.WriteLine(ones + "-" + tens + "-" + hundreds);
+            sum = ones + tens + hundreds;
+            Console.WriteLine(sum); 
+
+            #endregion
+            Console.Read();
+        }
+    }
+}
