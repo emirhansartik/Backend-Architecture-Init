338b109f2b28b958686d06f28ae8d0d4140585d7
Emirhan Sartik <sartikemirhan@gmail.com>
2026-07-29 21:50:15 +0300

    Practice

---
+++ 08_Methods/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Metotlar
+            //void CustomerList()
+            //{
+            //    Console.WriteLine("Ali Yıldız");
+            //    Console.WriteLine("Ayşe Yılmaz");
+            //    Console.WriteLine("Ferhat Akın"); 
+            //}
+
+            //CustomerList();
+            //CustomerList();
+            //CustomerList();
+            //CustomerList();
+            #endregion
+
+            #region Geriye Değer Döndürmeyen String Parametreli Metodlar 
+
+            //void WriteMethod(string CustomerName)
+            //{
+            //    Console.WriteLine(CustomerName);
+            //}
+
+            //WriteMethod("Mehmet");
+
+            //void CustomerCard(string name, string Surname)
+            //{
+            //    Console.WriteLine("Müşteri: " + name + " " + Surname);
+            //}
+
+            //CustomerCard("Mehmet", "Yıldırım");
+            //CustomerCard("Mehmet", "Yıldırım");
+
+            #endregion
+
+            #region Geriye Değer Döndürmeyen Int Parametreli Metodlar 
+
+            //void sum(int number1, int number2, int number3, int result)
+            //{
+            //    result = number1 + number2 + number3;
+            //    Console.WriteLine(result);
+            //}
+
+            //sum(1, 2, 3, 0);
+
+            #endregion
+
+            #region Geriye Değer Döndüren Metodlar 
+
+            //string CustomerName()
+            //{
+            //    return " Buse YIldırım";
+            //}
+
+            //CustomerName();
+
+            //string StudentCard()
+            //{
+            //    string name = "Ali";
+            //    string surname = "akbaba";
+
+
+            //    return name + " " + surname; 
+            //}
+
+            //Console.WriteLine(StudentCard());
+
+            #endregion
+
+            #region Geriye Değer Döndüren String Parametreli Metodlar 
+            //string CountryCard(string CountryName, string Capital,  string  FlagColor) {
+
+            //    string Cardİnfo = "Ülke:  " + CountryName + " -  Başkent: " + Capital + " -  Bayrak Rengi: " + FlagColor;
+            //    return Cardİnfo;
+            //}
+            //string x, y, z;
+            //Console.Write("Ülke Adını Gir: ");
+            //x = Console.ReadLine();
+
+            //Console.Write("Başkenti Giriniz: ");
+            //y = Console.ReadLine();
+
+            //Console.Write("Bayrak Rengini Giriniz: ");
+            //z = Console.ReadLine();
+
+
+            //Console.WriteLine(CountryCard(x,y,z));
+
+            #endregion
+
+
+            Console.Read();
+
+        }
+    }
+}
